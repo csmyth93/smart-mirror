f3a4c208f9fac7c76e9e4ec855adc8b62768b895
csmyth93 <c.smyth93@gmail.com>
2020-06-21 16:06:36 +0100

    added title for window and some text

--- clock.py
+++ clock.py
@@ -1,9 +1,18 @@
 import tkinter as tk
 
+# create the window
 root = tk.Tk()
+
+# add a title to the window
 root.title('Hello world!') #this will need removing at a later date
 
-label = tk.Label(root, text='from Callum Smyth')
+# add a label to display in the window
+label = tk.Label(root, text='Brad sucks eggs')
 label.pack()
+
+# create a set of options to display
+options = ['Option 1', 'Option 2', 'Option 3']
+select_option = tk.StringVar()
+select_option.set(options[0])
 
 root.mainloop()
