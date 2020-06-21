218707dd30999f1672af0215d69f644288b2ce64
csmyth93 <c.smyth93@gmail.com>
2020-06-21 15:47:08 +0100

    added new file for the display clock

---
+++ clock.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Hello world!') #this will need removing at a later date
+
+label = tk.Label(root, text='from Callum Smyth')
+label.pack()
+
+root.mainloop()
